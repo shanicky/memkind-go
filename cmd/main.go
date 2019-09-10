95158653d275a157152c5ea3832488af30f2270b
Chen <chen@zhihu.com>
2019-09-10 19:07:34 +0800

    update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,74 @@
-package cmd
+package main
+
+/*
+#include <stdio.h>
+static void p(char *s) {
+printf("buff is [%s]\n", s);
+}
+
+static void pl(char *s, int n) {
+for(int i = 0; i <n ;i++ ) {
+printf("%d ", s[i]);
+}
+
+printf("\n");
+
+}
+*/
+import "C"
+
+import (
+    "fmt"
+    "github.com/shanicky/memkind-go/memkind"
+    "os"
+    "reflect"
+    "unsafe"
+)
+
+
+func main() {
+    path := "/tmp"
+
+    if len(os.Args) > 2 {
+        fmt.Printf("Usage: %s [pmem_kind_dir_path]", os.Args[0])
+        os.Exit(1)
+    }
+
+    if len(os.Args) == 2 {
+        path = os.Args[1]
+    }
+
+    fmt.Printf("PMEM kind directory: %s\n", path);
+
+    var kind memkind.MemkindT = nil
+    if errno := memkind.MemkindCreatePmem(path, 1024*1024*1024*32, &kind); errno != 0 {
+        memkindPrintError(errno)
+        os.Exit(1)
+    }
+
+    size := uint(10)
+    ptr := memkind.MemkindMalloc(kind, size)
+    defer memkind.MemkindFree(kind, ptr)
+
+    bs := unsafePtrToBytes(ptr, size)
+    copy(bs[:10], []byte("12345678"))
+
+    printPtr(ptr)
+}
+
+func printPtr(ptr unsafe.Pointer) {
+    C.p((*C.char)(ptr))
+    C.pl((*C.char)(ptr), C.int(10))
+}
+
+func unsafePtrToBytes(ptr unsafe.Pointer, size uint) []byte {
+    return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+        Data: uintptr(ptr), Len: int(size), Cap: int(size),
+    }))
+}
+
+func memkindPrintError(errno int32) {
+    buf := make([]byte, 1024)
+    memkind.MemkindErrorMessage(errno, buf, 1024)
+    fmt.Printf("%s\n", buf)
+}
